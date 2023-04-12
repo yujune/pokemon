47be77d304442b57178490656b42405fee71fd68
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-04-12 08:05:51 +0800

    chore: Add getMaxStats helper function.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,32 @@ export const defaultTake = 20;
 export const pokemonImgBaseUrl = 'https://img.pokemondb.net/artwork/large/';
 export const windowWidth = Dimensions.get('window').width;
 export const windowHeight = Dimensions.get('window').height;
+
+//Due to api not providing max statistic
+export class MaxStat {
+  static hp = 255;
+  static attack = 190;
+  static defense = 250;
+  static specialAttack = 194;
+  static specialDefense = 250;
+  static speed = 200;
+
+  static getMaxStats(name: string): number {
+    switch (name) {
+      case 'hp':
+        return this.hp;
+      case 'attack':
+        return this.attack;
+      case 'defense':
+        return this.defense;
+      case 'special-attack':
+        return this.specialAttack;
+      case 'special-defense':
+        return this.specialDefense;
+      case 'speed':
+        return this.speed;
+      default:
+        return this.hp;
+    }
+  }
+}
