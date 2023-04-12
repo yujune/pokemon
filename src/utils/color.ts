90b87c136ce4d3f5aa7faab56fbebecb27980524
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-04-12 08:05:57 +0800

    chore: Add new colors.

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,4 +1,7 @@
 export class AppColor {
   static black = '#000000';
+  static transparentBlack = 'rgba(52, 52, 52, 0.8)';
   static white = '#ffffff';
+  static pattensBlue = '#E2F4FD';
+  static deepSkyBlue = '#007AFF';
 }
