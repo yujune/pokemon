adfd1f5636864d6d732d43b88a0a2afa3d1e2e53
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-04-14 07:36:11 +0800

    chore: Add reusable global style.

---
+++ src/utils/global-style.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {AppSpacing} from '../themes/theme';
+
+export const AppStyle = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    paddingHorizontal: AppSpacing.standard,
+    paddingVertical: AppSpacing.mediumSmall,
+  },
+});
