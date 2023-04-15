6af7bb362aa27ed12dd5bfa588b04583ffcd297b
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-04-15 10:12:37 +0800

    chore: Add new color for pokemon type.

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -5,4 +5,19 @@ export class AppColor {
   static pattensBlue = '#E2F4FD';
   static deepSkyBlue = '#007AFF';
   static seaShell = '#f0f0f0';
+  static orangeRed = '#F7786B';
+  static cyanBlue = '#77C4FE';
+  static mutedPurple = '#7C538C';
+  static greenishCyan = '#4FC1A6';
+  static yellowishGreen = '#92DF68';
+  static brightSun = '#FFCE4B';
+  static pinkishBrown = '#B1736C';
+  static periwinkleGrey = '#CDCDE6';
+  static paleOrange = '#f7af5a';
+  static orangeSalmon = '#be7447';
+  static dusk = '#405483';
+  static dawn = '#A2A29B';
+  static lavenderPurple = '#9473B4';
+  static columbiaBlue = '#A4DEF6';
+  static cloud = '#C5C5C5';
 }
