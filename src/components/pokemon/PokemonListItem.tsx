e99e4bfc7e2a28743ebba8429811521132cd5416
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-04-15 21:51:38 +0800

    chore: Support custom url for pokemon list item.

--- src/components/pokemon/PokemonListItem.tsx
+++ src/components/pokemon/PokemonListItem.tsx
@@ -2,28 +2,39 @@ import React, {FC} from 'react';
 import {Image, Pressable, Text} from 'react-native';
 import {Props} from './PokemonListItem.interface';
 import {style} from './PokemonListItem.style';
-import {getPokemonImageUrl} from '../../utils/general-helper';
+import {getPokemonImageUrl, isSvgUrl} from '../../utils/general-helper';
 import {useCustomTheme} from '../../context/theme/theme_provider';
+import {SvgUri} from 'react-native-svg';
 
 export const PokemonListItem: FC<Props> = ({
   name,
+  customUrl,
   onPressed,
   containerStyle,
 }) => {
   const {
     theme: {color},
   } = useCustomTheme();
+
+  const url = customUrl !== undefined ? customUrl : getPokemonImageUrl(name);
+
   return (
     <Pressable
       style={[style.container, containerStyle]}
       onPress={() => {
         onPressed?.(name);
       }}>
-      <Image
-        style={style.image}
-        resizeMode="contain"
-        source={{uri: getPokemonImageUrl(name)}}
-      />
+      {isSvgUrl(url) ? (
+        <SvgUri style={style.image} width={100} height={100} uri={url} />
+      ) : (
+        <Image
+          style={style.image}
+          resizeMode="contain"
+          source={{
+            uri: url,
+          }}
+        />
+      )}
       <Text style={[style.label, {color: color.text}]}>{name}</Text>
     </Pressable>
   );
