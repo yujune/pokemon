b06e6b6106a18787e98f77159153b5b34b8f3da9
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-04-19 22:06:01 +0800

    chore: Store user preferred theme in async storage.

--- src/context/theme/theme_provider.tsx
+++ src/context/theme/theme_provider.tsx
@@ -1,7 +1,8 @@
-import React, {FC, createContext, useContext, useState} from 'react';
+import React, {FC, createContext, useContext, useEffect, useState} from 'react';
 import {IThemProvider, IThemeContext} from './them_provider.interface';
 import {APP_DARK_THEME, APP_DEFAULT_THEME} from '../../themes/theme';
 import {ThemeType} from '../../themes/theme.interface';
+import {AsyncStorageHelper} from '../../utils/async-storage-helper';
 
 export const ThemeContext = createContext<IThemeContext>({
   theme: APP_DEFAULT_THEME,
@@ -15,8 +16,18 @@ export const ThemeProvider: FC<IThemProvider> = ({children}) => {
     if (themeType === type) {
       return;
     }
-    setThemeType(type);
+    AsyncStorageHelper.setTheme(type).then(() => {
+      setThemeType(type);
+    });
   };
+
+  useEffect(() => {
+    AsyncStorageHelper.getTheme().then(theme => {
+      if (theme !== null && theme.length > 0) {
+        setThemeType(theme as ThemeType);
+      }
+    });
+  }, []);
 
   return (
     <ThemeContext.Provider
